76d873053e05b0b6ef78d2a70a2eded230951485
Kyle Fox <kyle.fox@gmail.com>
2011-01-24 15:21:22 -0700

    minor stuffs

--- tt.js
+++ tt.js
@@ -1,6 +1,7 @@
 (function() {
   
   window.tt = {
+    VERSION: 0.1,
     DEFAULTS: {'line-height': '1.50em', 'font-size': '16px', color: '#444', background: '#fff'},
     LOREM: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
     ADJUST_LIGHTNESS: 0.1
