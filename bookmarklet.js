a62565cb2a358b37a5530498dc40a7f54c65e93f
Kyle Fox <kyle.fox@gmail.com>
2011-04-03 20:54:39 -0600

    STuff!

--- bookmarklet.js
+++ bookmarklet.js
@@ -3,12 +3,15 @@ function tt() {
     z = d.createElement('scr' + 'ipt'),
     c = d.createElement('link'),
     b = d.body;
+    
+    z.setAttribute('src', 'http://dl.dropbox.com/u/780754/typekit_tweaker/tt.js');
+    
+    c.setAttribute('href', 'http://dl.dropbox.com/u/780754/typekit_tweaker/tt.css');
+    c.setAttribute('rel', 'stylesheet');
+    
     try {
         if (!b) throw (0);
-        c.setAttribute('href', 'http://dl.dropbox.com/u/780754/typekit_tweaker/tt.css');
-        c.setAttribute('rel', 'stylesheet');
         d.head.appendChild(c);
-        z.setAttribute('src', 'http://dl.dropbox.com/u/780754/typekit_tweaker/tt.js');
         z.onload = function() {
           window.tt.tweaker = new tt.Tweaker($('.specimen-editor textarea'));
         };
